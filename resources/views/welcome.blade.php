cbee9453a2019ef5454716abcafb002c3c4c0cab
tkymori <tky.mori@foresight-inc.co.jp>
2022-06-07 16:39:44 +0900

    editUrl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="TWITTER_TITLE" />
-        <meta property="og:url" content="https://www.foresight.jp/?utm_medium=Direct&utm_source=test_app_manabun_2_1393405075129737217&utm_campaign=2022-06-06_15:02:10" />
+        <meta property="og:url" content="https://pracy.herokuapp.com/" />
         <meta property="og:title" content="OG_TITLE" />
         <meta property="og:description" content="OG_DESC" />
         <meta property="og:image" content="https://posty-v2.herokuapp.com/images/01.jpg" />
