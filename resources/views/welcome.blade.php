26850f99b5bed8beb64e8613a5038b77aad0741e
tkymori <tky.mori@foresight-inc.co.jp>
2022-06-08 09:17:23 +0900

    調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,23 +3,25 @@
     <head>
     <?php
         if (isset($_GET['utm_content']) && is_numeric($_GET['utm_content'])) {
-            $trigger = $_GET['utm_content'];
-            if ($trigger == 1) {
+            $type = $_GET['utm_content'];
+            if ($type == 1) {
                 $title = "1ですー";
                 $description = "1のdesc";
-            } else if ($trigger == 2) {
+            } else if ($type == 2) {
                 $title = "2ですー";
                 $description = "2のdesc";
             }
             ?>
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="TWITTER_TITLE" />
-                <meta property="og:url" content="https://posty-v2.herokuapp.com/" />
-                <meta property="og:title" content="{{$title}}" />
-                <meta property="og:description" content="{{$description}}" />
-                <meta property="og:image" content="https://posty-v2.herokuapp.com/images/{{$trigger}}.jpg" />
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
+            <!-- twitter cardのサイズ -->
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@yanb9999" />
+            <meta property="og:url" content="https://posty-v2.herokuapp.com/" />
+            <meta property="og:title" content="{{$title}}" />
+            <meta property="og:description" content="{{$description}}" />
+            <!-- 画像URL -->
+            <meta property="og:image" content="https://posty-v2.herokuapp.com/images/{{$type}}.jpg" />
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php } ?>
         <title>Laravel</title>
 
